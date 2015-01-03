7fdc15b0f04617e9a3c3e5c91febab80b7633999
longjames <824499468@qq.com>
2015-01-03 22:05:19 +0800

    0.99
    咳嗽感冒...

--- src/doors/QqClient/src/com/qq/client/tools/ChattingRecord.java
+++ src/doors/QqClient/src/com/qq/client/tools/ChattingRecord.java
@@ -10,10 +10,123 @@ public class ChattingRecord {
 	    private Message m;
 	    private String owner;
 	    private String friendID;
+	    public static void  WriteHistoryId(String idname){//将id跟新,有新ID输入时就跟新，而且最多只保存5个,
+	    	String idstr=idname;
+	        String []idn= idstr.split(" ",5);//按空格分隔
+	        String id="";
+	        for(int i=0;i<idn.length;i++){
+	        	  id = id +idn[i]+"\n";
+	        }
+	        File owner_file = new File ("F://Doors");//文件要一级一级的建立
+			File history_file= new File("F://Doors//"+"DoorsHistoryId.txt");
+			try{
+				if (!owner_file.exists()){//如果文件不存在就创建一个新文件
+					owner_file.mkdir();
+				} 
+	
+				if (!history_file.exists()){//如果文件不存在就创建一个新文件
+					history_file.createNewFile();
+				}
+				FileOutputStream fos = new FileOutputStream(history_file);
+				fos.write((id).getBytes("GBK"));
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+	        
+	    }
 	    
-	    public void WritePassword(String id,String password){
+	    
+	    //////
+	    public static String[] ReadHistroyId(){
+	    	String [] id={"1","2","3","4","5"};
 	    	File owner_file = new File ("F://Doors");//文件要一级一级的建立
-			File record_file= new File("F://Doors//"+"DoorsPassword。txt");
+			File history_file= new File("F://Doors//"+"DoorsHistoryId.txt");
+			try{
+				if (!owner_file.exists()){//如果文件不存在就创建一个新文件
+					owner_file.mkdir();
+				} 
+	
+				if (!history_file.exists()){//如果文件不存在就创建一个新文件
+					history_file.createNewFile();
+					return id;
+				}
+			FileReader fileReader= new FileReader(history_file);
+			BufferedReader bufferedReader=new  BufferedReader(fileReader);
+			String read = null;
+			String ids =null;
+		    while((read=bufferedReader.readLine())!=null){  //读消息,用空格分开
+		     ids=ids + " "+read;
+		    }
+		    if(ids!=null){
+		    id = ids.split(" ");
+		    }
+		    if(bufferedReader!=null){  
+			    bufferedReader.close();  
+			   }  
+			  if(fileReader!=null){  
+			    fileReader.close();
+			  }
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+	    	return id;
+	    }
+	    ///////在读的时候如果不存在就要判断，不然会空
+	    public static String ReadPassword(String userid){//账号 密码\n   的格式保存在文件中，将读出来的账号密码放在一个String里面，然后判断是否存在
+	    	String []passwordAndId=null;
+	    	String pai="";
+	    	String password="";
+	    	String id=userid;
+	    	File owner_file = new File ("F://Doors");//文件要一级一级的建立
+			File history_file= new File("F://Doors//"+"DoorsPassword.txt");
+			try{
+				if (!owner_file.exists()){//如果文件不存在就创建一个新文件
+					owner_file.mkdir();
+				} 
+	
+				if (!history_file.exists()){//如果文件不存在就创建一个新文件
+					history_file.createNewFile();
+					return password;
+				}
+			FileReader fileReader= new FileReader(history_file);
+			BufferedReader bufferedReader=new  BufferedReader(fileReader);
+			String read = null;
+			
+		    while((read=bufferedReader.readLine())!=null){  //读消息,用空格分开
+		    	pai=pai+read+" ";//账号+密码
+		    }
+		    System.out.println("pai+"+pai);
+		    if (pai!=null){
+		    	passwordAndId = pai.split(" ");//这样密码紧跟着账号，账号在偶数位 
+		    
+		    //判断是否曾经记住密码，
+		    for (int i=0;i<passwordAndId.length;i=i+2){
+		    	//System.out.println(i);
+		    	if(id.equals(passwordAndId[i])){
+		    		//即找到了曾经保存的密码，返回密码
+		    		password=passwordAndId[i+1];
+		    		i=passwordAndId.length;
+		    	}
+		    }
+		    }
+		    if(bufferedReader!=null){  
+			    bufferedReader.close();  
+			   }  
+			  if(fileReader!=null){  
+			    fileReader.close();
+			  }
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+	    	return password;
+	    }
+	    
+	    
+	    
+	    
+	    public static void WritePassword(String id,String password){//记住的密码
+	    	File owner_file = new File ("F://Doors");//文件要一级一级的建立
+			File record_file= new File("F://Doors//"+"DoorsPassword.txt");
 			try{
 				if (!owner_file.exists()){//如果文件不存在就创建一个新文件
 					owner_file.mkdir();
@@ -25,7 +138,7 @@ public class ChattingRecord {
 				//但是如果别人修改了密码怎么办，那也没法了，但是如果//之前点击过忘记密码之后又没有点击的话，没有点击记住密码的迹象往这个文件，并删除其数据
 				//但是如果数据已经保存过数据，又保存貌似不好，这得改？？？？？？？？？？？？
 				FileOutputStream fos = new FileOutputStream(record_file,true);
-				fos.write((id+"+"+password).getBytes("GBK"));
+				fos.write((id+" "+password).getBytes("GBK"));
 				fos.close();
 			}catch(Exception e){                                              
 				e.printStackTrace();
--- src/doors/QqClient/src/com/qq/client/view/QqClientLogin.java
+++ src/doors/QqClient/src/com/qq/client/view/QqClientLogin.java
@@ -14,7 +14,7 @@ import com.qq.common.User;
 
 import java.awt.*;
 import java.awt.event.*;
-public class QqClientLogin extends JFrame implements ActionListener,FocusListener{
+public class QqClientLogin extends JFrame implements ItemListener, ActionListener,FocusListener{
 
 	//定义北部需要的组件
 	
@@ -25,20 +25,23 @@ public class QqClientLogin extends JFrame implements ActionListener,FocusListene
 	JPanel jp2,jp3,jp4;
 	JLabel jp2_jbl1,jp2_jbl2;//,jp2_jbl4,jp2_jbl3
 	//JButton jp2_jb1;
-	JTextField jp2_jtf;//qq haoma
+	JComboBox jcbox=null;//JTextField jp2_jtf;//qq haoma
 	JPasswordField jp2_jpf;//mima
 	JCheckBox jp2_jcb2;//jp2_jcb1,
-	
-	
-	
+	//String[] id={"1","2","3","4","5"};//用来记录以往的id输入
+	//String [] historyId=null;//从文件中获取历史输入
+	String defaultId="luojun";//默认id，最近输入的id为默认id
+
 	//定义南部需要的组件
 	JPanel jp1;
 	JButton jp1_jb1,jp1_jb2,jp1_jb3;//最下面的3个
-	
+	String userid =null;//真正的id和密码
+	String password=null;
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		QqClientLogin qqClientLogin=new QqClientLogin();
-		
+	
 	}
 	
 	
@@ -52,20 +55,25 @@ public class QqClientLogin extends JFrame implements ActionListener,FocusListene
 		jp2=new JPanel(new GridLayout(3,2));//3X2
 		
 		jp2_jbl1=new JLabel("Doors号码",JLabel.CENTER);
-		jp2_jbl1.addFocusListener(this);//如果获得焦点，下拉出最近几个账号，离开焦点查询是否曾经记住密码
+		//jp2_jbl1.addFocusListener(this);//如果获得焦点，下拉出最近几个账号，离开焦点查询是否曾经记住密码
 		jp2_jbl2=new JLabel("Doors密码",JLabel.CENTER);
 		//jp2_jbl3=new JLabel("忘记密码",JLabel.CENTER);
 		//jp2_jbl3.setForeground(Color.blue);//设置颜色
 		//jp2_jbl4=new JLabel("申请密码保护",JLabel.CENTER);
 		//jp2_jb1=new JButton(new ImageIcon("image/clear.gif"));
-		jp2_jtf=new JTextField();
-		jp2_jpf=new JPasswordField();
+		jcbox=new JComboBox(ChattingRecord.ReadHistroyId());
+		jcbox.setEditable(true);//可编辑
+		ComboBoxEditor editor=jcbox.getEditor();//输入，就像TextFiled
+		jcbox.configureEditor(editor,defaultId);//默认id
+		jcbox.addItemListener(this);
+		//jcbox.addActionListener(this);
+		jp2_jpf=new JPasswordField(password);
 		//jp2_jcb1=new JCheckBox("隐身登录");
 		jp2_jcb2=new JCheckBox("记住密码");
 		
 		//把控件按照顺序加入到jp2
 		jp2.add(jp2_jbl1);
-		jp2.add(jp2_jtf);//qq号码 
+		jp2.add(jcbox);//qq号码 
 		//jp2.add(jp2_jb1);
 		////
 		jp2.add(jp2_jbl2);//qq 密码
@@ -112,15 +120,16 @@ public class QqClientLogin extends JFrame implements ActionListener,FocusListene
 		{
 			QqClientUser qqClientUser=new QqClientUser();
 			User u=new User();
-			u.setUserId(jp2_jtf.getText().trim());
+			u.setUserId(userid);
 			u.setPasswd(new String(jp2_jpf.getPassword()));
 			u.setState("1");//隐身登录的话就是2
 			
 			if(qqClientUser.checkUser(u))
 			{
-				//如果点击了记住密码，写入某个文件夹
+				//如果点击了记住密码，写入某个文件夹,这里待会再写,账号要记住吧
+			//	ChattingRecord.WriteHistoryId(u.getUserId());
 				if(jp2_jcb2.isSelected()){
-					
+					ChattingRecord.WritePassword(u.getUserId(),u.getPasswd());
 				}
 				JOptionPane.showMessageDialog(this,"登录成功");//登录成功会弹出
 				try {
@@ -174,4 +183,22 @@ public class QqClientLogin extends JFrame implements ActionListener,FocusListene
 		
 	}
 
+
+	@Override
+	public void itemStateChanged(ItemEvent e){//ItemListener界面只有itemStateChanged()一个方法，在此实作它。
+		   if (e.getStateChange()==ItemEvent.SELECTED){//这是选中的那项，因为状态改变有两种，一个没有选中，一个选中了
+		  try{
+		   userid=(String) e.getItem();//将得到的id到文件中查询一次，看是否保持文件,没有会返回空
+		  // password= "luojunniubi";
+		  password= ChattingRecord.ReadPassword(userid);
+		  System.out.println("password"+password);
+		  System.out.println("userid"+userid);
+		  System.out.println(e.getItem());
+		  jp2_jpf.setText(password);
+		   }finally{//
+		   
+		   }
+		   }
+		   }
+	
 }
